81b4906d6ad624a739d291913501f8f55d6d260c
ruchakhopkar <70127769+ruchakhopkar@users.noreply.github.com>
2021-12-10 19:00:02 -0500

    Update idl_project.py

--- idl_project.py
+++ idl_project.py
@@ -172,5 +172,5 @@ elif select_dept==l1[19]:
   vid = open(filename,  'rb')
   st.video(vid)
   
-print('coauthors: Rucha Khopkar, Shubhranshu Singh, Saahithya Yendluri, Kunal Dhawan')
-print('Feel free to reach us out on ruchakhopkar@gmail.com')
+st.markdown('coauthors: Rucha Khopkar, Shubhranshu Singh, Saahithya Yendluri, Kunal Dhawan')
+st.markdown('Feel free to reach us out on ruchakhopkar@gmail.com')
